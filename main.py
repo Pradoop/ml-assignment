69e2b1e08c8095ed8061f31bc8d34996040b2303
SteveDePicker <80755832+SteveDePicker@users.noreply.github.com>
2021-12-05 11:19:53 +0100

    skeleton for the project

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-#test comment
-#test comment v.2
+# test comment
+# test comment v.2
+
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("ford.csv")
+
+X = df[['model', 'year', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize']]
+y = df['price']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
