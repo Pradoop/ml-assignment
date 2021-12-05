7cca97408fd57e39373a130786791391f237980f
pradeep.sureshkumar <pradeep.sureshkumar@student.kuleuven.be>
2021-12-05 16:02:27 +0100

    modified file and tried lin regression

--- lin_reg_not_normalized.py
+++ lin_reg_not_normalized.py
@@ -1,10 +1,16 @@
 import sys
-import pandas
+import numpy as np
+import matplotlib.pyplot as plt  # To visualize
+import pandas  # To read data
 from sklearn import linear_model
-import random
 
 # file opening
-data = pandas.read_csv(sys.path[0] + "/files/ford.csv", sep=';')
-print(data)
+dataframe = pandas.read_csv(sys.path[0] + "/files/ford.csv", sep=';')
+print(dataframe)
 
+x = dataframe[['model', 'year', 'engineSize', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg']]
+y = dataframe['price']
+
+regr = linear_model.LinearRegression()
+regr.fit(x, y)
 
