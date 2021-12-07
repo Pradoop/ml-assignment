606f8911a62a6a31bdb694db4d641f3455fbf242
SteveDePicker <80755832+SteveDePicker@users.noreply.github.com>
2021-12-07 16:10:42 +0100

    tried to make DataFrame

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 # test comment
 # test comment v.2
 
-import pandas
+import pandas as pd
 from sklearn import linear_model
+import matplotlib.pyplot as plt
 
-df = pandas.read_csv("ford.csv")
+if __name__ == '__main__':
+    ford = pd.read_csv("ford.csv", header = 0)
+    #usecols = ['model','year','price','transmission','mileage','fuelType','tax','mpg','engineSize']
+    #X = ford[['model', 'year', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize']]
+    #y = ford['price']
 
-X = df[['model', 'year', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize']]
-y = df['price']
+    print(ford)
 
-regr = linear_model.LinearRegression()
-regr.fit(X, y)
+    df = pd.DataFrame(data = ford, columns = ['model', 'year', 'price', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize'])
+
+    #columns = ['model', 'year', 'price', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize')
+
+    print(df)
+
+    #X = ford[['model', 'year', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize']]
+    #y = ford['price']
+
+    #print(df['mileage'])
+    #print(df['price'])
+    #plt.scatter(df['mileage'], df['price'], color = 'red')
+    #plt.title('mileage Vs price', fontsize = 14)
+    #plt.xlabel('mileage', fontsize=14)
+    #plt.ylabel('price', fontsize = 14)
+    #plt.grid(True)
+    #plt.show()
+
+# regr = linear_model.LinearRegression()
+# regr.fit(X, y)
