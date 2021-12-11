e257033dc7fb2720e89b38217a18cfbb4d836265
pradeep.sureshkumar <pradeep.sureshkumar@student.kuleuven.be>
2021-12-11 11:07:44 +0100

    not normalized version

--- lin_reg_not_normalized.py
+++ lin_reg_not_normalized.py
@@ -9,29 +9,35 @@ from sklearn.model_selection import train_test_split
 # file opening
 dataframe = pandas.read_csv(sys.path[0] + "/files/ford.csv", sep=';')
 
+# separating features dataset
 x = dataframe[['model', 'year', 'engineSize', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg']]
 y = dataframe['price']
 
+# splitting dataset to training and test
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=100)
-
 regr = linear_model.LinearRegression()
+
+# fit is essentially training your model
 regr.fit(x_train, y_train)
 
+# this gives us the values for the intercept and the coefficient for each feature
 print("Intercept: ", regr.intercept_)
-print("Coefficients: ")
-print(list(zip(x, regr.coef_)))
+print("Coefficients: ", list(zip(x, regr.coef_)))
 
+# for test set
 y_predicted_result = regr.predict(x_test)
+print("Predicted price values: ", list(map('{:.2f}'.format, y_predicted_result)))
 
-print("Prediction for test set: {}".format(y_predicted_result))
+# comparison
+comparison = pandas.DataFrame({'Training value': y_test, 'Predicted value': y_predicted_result})
+print(comparison.head())
 
-mlr_diff = pandas.DataFrame({'Actual value': y_test, 'Predicted value': y_predicted_result})
-mlr_diff.head()
-
+# Evaluation
 meanAbErr = metrics.mean_absolute_error(y_test, y_predicted_result)
 meanSqErr = metrics.mean_squared_error(y_test, y_predicted_result)
-rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_predicted_result, y_predicted_result))
-print('R squared: {:.2f}'.format(regr.score(x,y)*100))
-print('Mean Absolute Error:', meanAbErr)
-print('Mean Square Error:', meanSqErr)
-print('Root Mean Square Error:', rootMeanSqErr)
+rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_test, y_predicted_result))
+
+print('R squared: {:.2f}'.format(regr.score(x, y)*100))
+print('Mean Absolute Error: {:.2f}', meanAbErr)
+print('Mean Square Error: {:.2f}', meanSqErr)
+print('Root Mean Square Error: {:.2f}', rootMeanSqErr)
