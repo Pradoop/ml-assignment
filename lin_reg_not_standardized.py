366ca859cff2e1827ace85d34cd92acd995abf10
pradeep.sureshkumar <pradeep.sureshkumar@student.kuleuven.be>
2021-12-11 15:06:20 +0100

    not normalized version

--- lin_reg_not_standardized.py
+++ lin_reg_not_standardized.py
@@ -1,31 +1,34 @@
 import sys
-
 import numpy
 import numpy as np
-import matplotlib.pyplot as plt  # To visualize
+from matplotlib import pyplot as plt
 import pandas  # To read data
 from sklearn import linear_model
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
 
 # file opening
 dataframe = pandas.read_csv(sys.path[0] + "/files/ford.csv", sep=';')
-dataframe = dataframe.sample(frac=1).reset_index(drop=True)
+#dataframe = dataframe.sample(frac=1).reset_index(drop=True)
 
 # separating features dataset
 x = dataframe[['model', 'year', 'engineSize', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg']]
 y = dataframe['price']
 
-# splitting dataset to training and test
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=100)
+# splitting dataset to training, validation, test and calculate scores for cross validation
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100)
 regr = linear_model.LinearRegression()
 
 # fit is essentially training your model
 regr.fit(x_train, y_train.values)
 
-# this gives us the values for the intercept and the coefficient for each feature
+# testSet intercept and coefficients
 print("Intercept: ", regr.intercept_)
 print("Coefficients: ", list(zip(x, regr.coef_)))
+
+# crossValidation scores
+scores = cross_val_score(regr, x_train, y_train, cv=5)
+print("Cross-validated scores:", scores)
 
 # for test set
 y_predicted_result = regr.predict(x_test)
@@ -35,7 +38,7 @@ print("Predicted price values: ", list(map('{:.2f}'.format, y_predicted_result))
 comparison = pandas.DataFrame({'Actual test value': y_test, 'Predicted value': y_predicted_result})
 print(comparison.head())
 
-# Evaluation
+# Evaluation metrics
 meanAbErr = metrics.mean_absolute_error(y_test, y_predicted_result)
 meanSqErr = metrics.mean_squared_error(y_test, y_predicted_result)
 rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_test, y_predicted_result))
