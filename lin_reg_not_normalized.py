ef64ccaace5d44305127e37733a5e10bd3d422e5
pradeep.sureshkumar <pradeep.sureshkumar@student.kuleuven.be>
2021-12-11 10:22:11 +0100

    not normalized version

--- lin_reg_not_normalized.py
+++ lin_reg_not_normalized.py
@@ -3,14 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt  # To visualize
 import pandas  # To read data
 from sklearn import linear_model
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 
 # file opening
 dataframe = pandas.read_csv(sys.path[0] + "/files/ford.csv", sep=';')
-print(dataframe)
 
 x = dataframe[['model', 'year', 'engineSize', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg']]
 y = dataframe['price']
 
-regr = linear_model.LinearRegression()
-regr.fit(x, y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=100)
 
+regr = linear_model.LinearRegression()
+regr.fit(x_train, y_train)
+
+print("Intercept: ", regr.intercept_)
+print("Coefficients: ")
+print(list(zip(x, regr.coef_)))
+
+y_predicted_result = regr.predict(x_test)
+
+print("Prediction for test set: {}".format(y_predicted_result))
+
+mlr_diff = pandas.DataFrame({'Actual value': y_test, 'Predicted value': y_predicted_result})
+mlr_diff.head()
+
+meanAbErr = metrics.mean_absolute_error(y_test, y_predicted_result)
+meanSqErr = metrics.mean_squared_error(y_test, y_predicted_result)
+rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_predicted_result, y_predicted_result))
+print('R squared: {:.2f}'.format(regr.score(x,y)*100))
+print('Mean Absolute Error:', meanAbErr)
+print('Mean Square Error:', meanSqErr)
+print('Root Mean Square Error:', rootMeanSqErr)
